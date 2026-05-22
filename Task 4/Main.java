ed3975ca3db5bc23d02fa520c3416a816b1411dc
Ali <alihassan786.ku@gmail.com>
2026-05-22 11:51:04 +0500

    Task 4

---
+++ Task 4/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+
+    public static void main(String[] args){
+
+        Integer num=6; //AUTOBOXING INTEGER VALUE 6
+
+        System.out.println("Wrapper class objext: "+num);
+
+        int num2=num;
+
+        System.out.println("Primitive data type int: "+num2);
+
+
+
+
+    }
+}
